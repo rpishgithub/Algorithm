59d0977ef22578b119d6bfbfd311c01d502fb1a6
rpish <rpishmail@gmail.com>
2022-05-07 17:28:31 +0800

    ReverseLinkedList

---
+++ ReverseLinkedList/src/main/java/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+public class ReverseLinkedList {
+    public static ListNode reverseLinkedList(ListNode head) {
+        ListNode prev=null;
+        ListNode next=null;
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public static void traverseLinkedList(ListNode head) {
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        traverseLinkedList(listNode1);
+        traverseLinkedList(reverseLinkedList(listNode1));
+    }
+}
